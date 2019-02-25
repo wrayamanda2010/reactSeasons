350f6e8902fd1e749f6ad097880ef9d1654a11e4
awray <amandawray2010@gmail.com>
2019-02-24 22:15:14 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+
+class App extends React.Component{
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
